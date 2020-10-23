fc4a482db9d0313083723e9e094ceb24ab5af7be
vitor0p9f <Vitor.Fontes002@gmail.com>
2020-10-22 21:42:33 -0300

    feat(global): preparando o repositorio para deploy no heroku

--- src/config/database.js
+++ src/config/database.js
@@ -9,5 +9,5 @@ module.exports={
   define:{
     timestamps: true,
     underscored: true
-  },
+  }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 const Express = require('express');
 const LoginController = require('./controllers/LoginController');
-const RegisterController = require('./controllers/RegisterController');
+const FrequencyController = require('./controllers/FrequencyController');
 
 const route = Express.Router();
 
-route.get('/register/:id/:mode_id/:course_id/:class_id/:matterIdentifier/:classQuanty', RegisterController.save);
+route.get('/register/:id/:mode_id/:course_id/:class_id/:matterIdentifier/:classQuanty', FrequencyController.save);
 route.post('/login',LoginController.login);
 
 module.exports = route;
