58ac118c9c5a191a126624c04436f9625fc09728
vitor0p9f <Vitor.Fontes002@gmail.com>
2020-11-10 22:56:41 -0300

    feat(pages): buscando dados do usuario
    Personalizando a pagina de perfil do usuario com as informaçoes vindas da api assim que o componente
    e carregado ( ainda nao esta pronto )

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import Styles from './styles.module.css';
 import Avatar from '../../assets/icons/Avatar.svg';
+import Api from '../../services/api';
+import AuthContext from '../../contexts/auth';
+
+interface DataProps{
+  name: string;
+}
+
+interface UserData{
+  matters: Array<DataProps> | undefined;
+  courses: Array<DataProps> | undefined;
+  classes: Array<DataProps> | undefined;
+  registration: string | undefined;
+  course: DataProps | undefined;
+  mode: DataProps | undefined;
+  schoolClass: DataProps | undefined;
+  shift: DataProps | undefined;
+}
 
 const Profile: React.FC = () => {
+  const {userInfo} = useContext(AuthContext);
+  const [userData,setUserData] = useState<UserData>({} as UserData);
+  const userName = userInfo.name;
+  const userType = userInfo.type;
+
+  useEffect(() =>{
+    async function fetchData(){
+      const {data} = await Api.get('/data/user');
+
+      if(data){
+        setUserData(data);
+      }
+    }
+
+    fetchData();
+  },[]);
+
   return (
     <>
       <Header />
       <div className={Styles.container}>
         <aside className={Styles.asideContainer}>
           <img src={Avatar} alt="Avatar"/>
-          <h1>Vitor Pereira Fontes</h1>
+          <h1>{userName}</h1>
           <div className={Styles.userData}>
-            <h3>Matrícula:</h3>
-            <h3>Curso:</h3>
-            <h3>Turma:</h3>
-            <h3>Modalidade:</h3>
-            <h3>Turno:</h3>
+            {userType === 'teacher' ? <>
+              
+            </> : <>
+              <h3>Matrícula: {userData.registration}</h3>
+              <h3>Curso: {userData.course?.name}</h3>
+              <h3>Turma: {userData.schoolClass?.name}</h3>
+              <h3>Modalidade: {userData.mode?.name}</h3>
+              <h3>Turno: {userData.shift?.name}</h3>
+            </>}
           </div>
         </aside>
         <section className={Styles.contentContainer}>
