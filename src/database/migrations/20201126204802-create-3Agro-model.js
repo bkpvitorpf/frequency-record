25f1af9d289056ef09e4d707d61867cf9b36e8b1
vitor0p9f <Vitor.Fontes002@gmail.com>
2020-11-26 18:07:02 -0300

    feat(database): criando novas tabelas na base de dados

---
+++ src/database/migrations/20201126204802-create-3Agro-model.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('agro_3',{
+      id:{
+        type: Sequelize.INTEGER, 
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      month:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      student_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: 'students', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      quimica:{
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      total_quimica:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      created_at:{
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at:{
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('agro_3');
+  }
+};
