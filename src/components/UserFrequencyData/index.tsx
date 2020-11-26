51aea22917088d63d2f1676d4a90a1a3c5e5f9fc
vitor0p9f <Vitor.Fontes002@gmail.com>
2020-11-26 17:34:08 -0300

    feat(components): finalizando estilizaçao de componente
    Finalizando a estilizaçao do componente que mostra o resultado das busca de alunos e professores no
    formulario ( finalizando a estilizaçao da tela do professor )

--- src/components/UserFrequencyData/index.tsx
+++ src/components/UserFrequencyData/index.tsx
@@ -18,18 +18,21 @@ interface IFrequencyData{
   courseName: string;
   className: string;
   shift: string;
+  monthlyClassesTaught: number;
   monthlyAttendedClasses: number;
   monthlyClasses: number;
   monthlyRemainingClasses: number;
   percentFrequency: number;
   anualRemainingClasses: number;
-  studentsList: StudentsFrequency;
+  studentsList: Array<StudentsFrequency>;
 }
 
 const UserFrequencyData: React.FC<any> = ({frequencyData}) => {
   const {userInfo} = useContext(AuthContext);
   const userFrequencyData: IFrequencyData = frequencyData;
   const date = new Date();
+
+  console.log(frequencyData);
 
   // Animação da barra de progresso da disciplina
   const progressBarAnimation = keyframes`
@@ -60,36 +63,51 @@ const UserFrequencyData: React.FC<any> = ({frequencyData}) => {
         <div className={Styles.container}>
           {userInfo.type === 'teacher' ? <>
             <div className={Styles.teacherContainer}>
-              <h3>{userFrequencyData.modeName}</h3>
-              <h3>{userFrequencyData.matterName}</h3>
-              <div className={Styles.info}>
-                <div className={Styles.box1}>
-                  <h3>Turma: {userFrequencyData.className}</h3>
-                  <h3>Mês: {userFrequencyData.month}</h3>
+              <div className={Styles.classData}>
+                <h3>{userFrequencyData.modeName}</h3>
+                <h3>{userFrequencyData.matterName}</h3>
+                <div className={Styles.info}>
+                  <div className={Styles.box}>
+                    <h3>Turma: {userFrequencyData.className}</h3>
+                    <h3>Mês: {userFrequencyData.month}</h3>
+                  </div>
+                  <div className={Styles.box}>
+                    <h3>Curso: {userFrequencyData.courseName}</h3>
+                    <h3>Turno: {userFrequencyData.shift}</h3>
+                  </div>
                 </div>
-                <div className={Styles.box2}>
-                  <h3>Curso: {userFrequencyData.courseName}</h3>
-                  <h3>Turno: {userFrequencyData.shift}</h3>
+                <div className={Styles.classInfo}>
+                  <div className={Styles.monthlyAttendedClasses}>
+                    <h3>Aulas ministradas</h3>
+                    <h3>{userFrequencyData.monthlyClassesTaught}</h3>
+                  </div>
+                  <div className={Styles.monthlyClasses}>
+                    <h3>Total de aulas</h3>
+                    <h3>{userFrequencyData.monthlyClasses}</h3>
+                  </div>
+                </div>
+                <div className={Styles.progressBarContainer}>
+                  <div className={Styles.background}>
+                    <ProgressBar/>
+                  </div>
+                  <h3>{userFrequencyData.percentFrequency} %</h3>
+                </div>
+                <div className={Styles.generalInfo}>
+                  <h3>Restam {userFrequencyData.monthlyRemainingClasses} aulas de {userFrequencyData.matterName} para serem ministradas em {userFrequencyData.month} e {userFrequencyData.anualRemainingClasses} em {date.getFullYear()}</h3>
                 </div>
               </div>
-              <div className={Styles.classInfo}>
-                <div className={Styles.monthlyAttendedClasses}>
-                  <h3>Aulas ministradas</h3>
-                  <h3>{userFrequencyData.monthlyAttendedClasses}</h3>
+              <div className={Styles.studentsList}>
+                <div className={Styles.header}>
+                  <h3>Frequência mensal da turma</h3>
                 </div>
-                <div className={Styles.monthlyClasses}>
-                  <h3>Total de aulas</h3>
-                  <h3>{userFrequencyData.monthlyClasses}</h3>
+                <div className={Styles.content}>
+                  {userFrequencyData.studentsList.map(student=>(
+                    <div className={Styles.studentData}>
+                      <h3>{student.name}</h3>
+                      <h3>{student.monthlyAttendedClasses} / {student.monthlyClasses}</h3>
+                    </div>
+                  ))}
                 </div>
-              </div>
-              <div className={Styles.progressBarContainer}>
-                <div className={Styles.background}>
-                  <ProgressBar/>
-                </div>
-                <h3>{userFrequencyData.percentFrequency} %</h3>
-              </div>
-              <div className={Styles.generalInfo}>
-                <h3>Restam {userFrequencyData.monthlyRemainingClasses} aulas de {userFrequencyData.matterName} para serem ministradas em {userFrequencyData.month} e {userFrequencyData.anualRemainingClasses} em {date.getFullYear()}</h3>
               </div>
             </div>
           </>: <>
